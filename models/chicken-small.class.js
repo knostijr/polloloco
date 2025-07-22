4757a2b46d2906010ef0a810ab2b70fa9e96ff13
knostijr <christophkonst@icloud.com>
2025-07-22 15:54:12 +0200

    small chicken animation

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -1,5 +1,32 @@
 class ChickenSmall extends MoveAbleObject {
+    y = 310;
+    height = 50;
+    width = 30;
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
 
-    
+
+    constructor() {
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 200 + Math.random() * 500;
+        this.speed = 0.15 + Math.random() * 0.5;
+        this.animate();
+    }
+
+
+    animate() {
+        this.moveLeft();
+
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+    }
+
 
 }
